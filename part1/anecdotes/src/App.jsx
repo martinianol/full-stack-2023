92cc94eac5929178469ece28fbaa92679bd12d7a
martiniano <martiniano.leguizamon@bluealba.com>
2023-08-21 17:46:17 -0300

    Exercise 1.13: anecdotes step2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -6,28 +6,52 @@ function randomIntFromInterval(min, max) {
 }
 
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
+  const originalAnecdotes = [
+    { text: "If it hurts, do it more often.", votes: 10 },
+    {
+      text: "Adding manpower to a late software project makes it later!",
+      votes: 2,
+    },
+    {
+      text: "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+      votes: 3,
+    },
+    {
+      text: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+      votes: 0,
+    },
+    { text: "Premature optimization is the root of all evil.", votes: 1 },
+    {
+      text: "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+      votes: 4,
+    },
+    {
+      text: "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+      votes: 3,
+    },
+    { text: "The only way to go fast, is to go well.", votes: 2 },
   ];
 
   const [selected, setSelected] = useState(0);
+  const [anecdotes, setAnecdotes] = useState(originalAnecdotes);
 
-  const handleClick = () => {
+  const handleNextAnecdote = () => {
     const rnd = randomIntFromInterval(0, anecdotes.length - 1);
     setSelected(rnd);
   };
 
+  const handleVote = () => {
+    const updatedAnecdotes = [...anecdotes];
+    updatedAnecdotes[selected].votes += 1;
+    setAnecdotes(updatedAnecdotes);
+  };
+
   return (
     <>
-      <div>{anecdotes[selected]}</div>
-      <button onClick={handleClick}>next anecdote</button>
+      <div>{anecdotes[selected].text}</div>
+      <div>has {anecdotes[selected].votes} votes</div>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNextAnecdote}>next anecdote</button>
     </>
   );
 };
