bbf19e2a9072af7fa03753d92ce035437aa1e786
martiniano <martiniano.leguizamon@bluealba.com>
2023-08-21 16:37:24 -0300

    Exercise 1.7 unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,10 +9,30 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(good + neutral + bad);
+  const [average, setAverage] = useState(
+    (good * 1 + neutral * 0 + bad * -1) / all
+  );
+  const [positive, setPositive] = useState(good / all);
 
-  const handleGood = () => setGood(good + 1);
-  const handleNeutral = () => setNeutral(neutral + 1);
-  const handleBad = () => setBad(bad + 1);
+  const handleGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+    setAverage((good + 1 - bad) / (all + 1));
+    setPositive((good + 1) / (all + 1));
+  };
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+    setAverage((good - bad) / (all + 1));
+    setPositive((good) / (all + 1));
+  };
+  const handleBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+    setAverage((good - bad - 1) / (all + 1));
+    setPositive((good) / (all + 1));
+  };
 
   return (
     <div>
@@ -24,6 +44,11 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {!isNaN(average) ? average : "no data yet"}</p>
+      <p>
+        postitive {!isNaN(positive) ? `${positive * 100}%` : "no data yet"}
+      </p>
     </div>
   );
 };
