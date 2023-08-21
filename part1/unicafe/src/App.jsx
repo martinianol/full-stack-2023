e29e8f4b16ded34784f60de37b7bb18f0bb5079e
martiniano <martiniano.leguizamon@bluealba.com>
2023-08-21 17:23:15 -0300

    Exercise 1.11: unicafe step6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,25 +5,35 @@ const Button = ({ handleClick, text }) => {
 };
 
 const StatisticLine = ({ text, value }) => (
-  <p>
-    {text} {value}
-  </p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 );
 
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   if (all === 0) return <p>No feedback given</p>;
+
   return (
-    <>
-      <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} />
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>Statistics</th>
+        </tr>
+      </thead>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
   );
 };
+
+const getPercentage = (val, total) => (val / total) * 100 + "%";
 
 const App = () => {
   // save clicks of each button to its own state
@@ -34,25 +44,25 @@ const App = () => {
   const [average, setAverage] = useState(
     (good * 1 + neutral * 0 + bad * -1) / all
   );
-  const [positive, setPositive] = useState(good / all);
+  const [positive, setPositive] = useState(getPercentage(good, all));
 
   const handleGood = () => {
     setGood(good + 1);
     setAll(all + 1);
     setAverage((good + 1 - bad) / (all + 1));
-    setPositive((good + 1) / (all + 1));
+    setPositive(getPercentage(good + 1, all + 1));
   };
   const handleNeutral = () => {
     setNeutral(neutral + 1);
     setAll(all + 1);
     setAverage((good - bad) / (all + 1));
-    setPositive(good / (all + 1));
+    setPositive(getPercentage(good, all + 1));
   };
   const handleBad = () => {
     setBad(bad + 1);
     setAll(all + 1);
     setAverage((good - bad - 1) / (all + 1));
-    setPositive(good / (all + 1));
+    setPositive(getPercentage(good, all + 1));
   };
 
   return (
