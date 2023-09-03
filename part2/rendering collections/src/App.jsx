5bba19a6691a406f2852a025befe01bb29576046
martiniano <martiniano.leguizamon@bluealba.com>
2023-09-03 17:26:55 -0300

    Controlled Component

--- part2/rendering collections/src/App.jsx
+++ part2/rendering collections/src/App.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import Note from "./Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +31,10 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
