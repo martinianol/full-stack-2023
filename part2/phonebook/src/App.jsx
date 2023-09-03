acb5aa7868aca1317fbf11505b9a272d0ab79f95
martiniano <martiniano.leguizamon@bluealba.com>
2023-09-03 18:06:20 -0300

    The Phonebook Step 4 - Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
+const originalPersons = [
+  { name: "Arto Hellas", number: "040-123456", id: 1 },
+  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+];
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
-  ]);
+  const [persons, setPersons] = useState(originalPersons);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filteredName, setFilteredName] = useState("");
+
+  const handleFilterChange = (event) => {
+    setFilteredName(event.target.value);
+  };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -38,9 +47,21 @@ const App = () => {
     }
   };
 
+  const personsToShow =
+    filteredName === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filteredName.toLowerCase())
+        );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <span>filter shown with</span>
+        <input value={filteredName} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new Person</h2>
       <form onSubmit={handleSaveName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -53,8 +74,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}><span>{person.name}</span> <span>{person.number}</span></p>
+      {personsToShow.map((person) => (
+        <p key={person.name}>
+          <span>{person.name}</span> <span>{person.number}</span>
+        </p>
       ))}
     </div>
   );
