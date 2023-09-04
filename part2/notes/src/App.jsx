379862c0cc47d35dcca32a728dbc7f5d3f9c6734
martiniano <martiniano.leguizamon@bluealba.com>
2023-09-03 22:12:19 -0300

    Improve Error Message

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
-import Note from "./Note";
+import Note from "./components/Note";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const getNotes = () => {
     noteService.getAll().then((initialNotes) => {
@@ -42,9 +44,12 @@ const App = () => {
       )
       .catch(error => {
         console.error(error)
-        alert(
-          `the note '${note.content}'was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   };
@@ -54,6 +59,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
