334c389f57b7fc3458c909d1e62c531e605b8dbc
martiniano <martiniano.leguizamon@bluealba.com>
2023-09-17 17:58:30 -0300

    Exercise 3.12

--- part3/notes-service/mongo.js
+++ part3/notes-service/mongo.js
@@ -1,24 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-`mongodb+srv://marsleguizamon:${password}@cluster0.bkdmqsz.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://marsleguizamon:${password}@cluster0.bkdmqsz.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
 /* const note = new Note({
   content: 'Mongoose makes things easy',
@@ -30,9 +29,9 @@ note.save().then(result => {
   mongoose.connection.close()
 }) */
 
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
-  mongoose.connection.close()
-})
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
