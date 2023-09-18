439771a125cfbccf8816827e3a06545fa55c79f7
martiniano <martiniano.leguizamon@bluealba.com>
2023-09-17 21:13:30 -0300

    Validators

--- part3/notes-service/index.js
+++ part3/notes-service/index.js
@@ -48,7 +48,7 @@ app.get("/api/notes/:id", (request, response, next) => {
   return maxId + 1;
 }; */
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
   if (!body.content) {
     return response.status(400).json({
@@ -61,21 +61,28 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
   const id = request.params.id;
-  const body = request.body;
+  const { content, important } = request.body;
 
   const note = {
-    content: body.content,
-    important: body.important,
+    content,
+    important,
   };
 
-  Note.findByIdAndUpdate(id, note, { new: true })
+  Note.findByIdAndUpdate(id, note, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -106,6 +113,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
