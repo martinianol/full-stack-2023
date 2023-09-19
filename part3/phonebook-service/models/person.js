b8032978bb3cee9e86f9b7fad4a5413fb95c3fec
martiniano <martiniano.leguizamon@bluealba.com>
2023-09-19 07:54:12 -0300

    ESLINT and validation

--- part3/phonebook-service/models/person.js
+++ part3/phonebook-service/models/person.js
@@ -17,7 +17,17 @@ mongoose
 
 const phonebookSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 phonebookSchema.set("toJSON", {
