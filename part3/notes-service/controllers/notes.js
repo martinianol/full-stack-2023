2a11baf92c22714734bae6f155003c0a7ea97451
martiniano <martiniano.leguizamon@bluealba.com>
2023-09-29 08:44:10 -0300

    part4-8

--- part3/notes-service/controllers/notes.js
+++ part3/notes-service/controllers/notes.js
@@ -1,18 +1,19 @@
 const Note = require("../models/note");
+const User = require("../models/user");
 const notesRouter = require("express").Router();
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
-notesRouter.get("/:id", async (request, response, next) => {
+notesRouter.get("/:id", async (request, response) => {
   const id = request.params.id;
   const note = await Note.findById(id);
   note ? response.json(note) : response.status(404).end();
 });
 
-notesRouter.post("/", async (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
   if (!body.content) {
     return response.status(400).json({
@@ -20,16 +21,22 @@ notesRouter.post("/", async (request, response, next) => {
     });
   }
 
+  const user = await User.findById(body.userId);
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
 
-notesRouter.put("/:id", async (request, response, next) => {
+notesRouter.put("/:id", async (request, response) => {
   const { id } = request.params;
   const { content, important } = request.body;
 
