31f7a6d56a235e36b6ba9913d4cb2a08fd7852bf
martiniano <martiniano.leguizamon@bluealba.com>
2023-10-22 16:56:26 -0300

    5.7 Blog list frontend, step7

--- part4/bloglist-frontend/src/components/Blog.jsx
+++ part4/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const [showDetails, setShowDetails] = useState(false);
+
+  return (
+    <>
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? "Hide" : "View"}
+        </button>
+        {showDetails && (
+          <>
+            <div>
+              <a>{blog.url}</a>
+            </div>
+            <div>
+              Likes {blog.likes} <button>like</button>
+            </div>
+            {blog.user && <div>{blog.user?.name}</div>}
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Blog;
