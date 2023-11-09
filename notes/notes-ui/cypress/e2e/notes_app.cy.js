add5b1a60a7e601233dcc635ce3d678ab87e0bbc
martiniano <martiniano.leguizamon@bluealba.com>
2023-11-09 06:20:28 -0300

    End to end testing - Controlling the state of the database

--- notes/notes-ui/cypress/e2e/notes_app.cy.js
+++ notes/notes-ui/cypress/e2e/notes_app.cy.js
@@ -1,5 +1,12 @@
 describe("Note app", () => {
   beforeEach(function () {
+    cy.request("POST", "http://localhost:3001/api/testing/reset");
+    const user = {
+      name: "Test Mosby",
+      username: "test",
+      password: "1234",
+    };
+    cy.request("POST", "http://localhost:3001/api/users/", user);
     cy.visit("http://localhost:5173");
   });
 
@@ -25,5 +32,37 @@ describe("Note app", () => {
     cy.get("#login-button").click();
 
     cy.contains("Test Mosby logged in");
+  });
+
+  describe("when logged in", function () {
+    beforeEach(function () {
+      cy.contains("Login").click();
+      cy.get("#username").type("test");
+      cy.get("#password").type("1234");
+      cy.get("#login-button").click();
+    });
+
+    it("a new note can be created", function () {
+      cy.contains("new note").click();
+      cy.get("#note-input").type("a note created by cypress");
+      cy.contains("save").click();
+      cy.contains("a note created by cypress");
+    });
+
+    describe("and a note exists", function () {
+      beforeEach(function () {
+        cy.contains("new note").click();
+        cy.get("#note-input").type("another note cypress");
+        cy.contains("save").click();
+      });
+
+      it("it can be made not important", function () {
+        cy.contains("another note cypress")
+          .contains("make not important")
+          .click();
+
+        cy.contains("another note cypress").contains("make important");
+      });
+    });
   });
 });
